373f82cad8c4b1be708989a0f044fd23c15c1343
suzumiyayuhi <923051761@qq.com>
2017-06-02 09:37:23 +0800

    2017.6.2
    2017.6.2

---
+++ hdoj/ACM_Steps/chapter4/section1/猜数字.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+using namespace std;
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n;
+		cin>>n;
+		int ans=pow(2,n)-1;
+		cout<<ans<<endl;
+	}
+} 
