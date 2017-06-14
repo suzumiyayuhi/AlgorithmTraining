e5d35ebc33dfb15ff5b26fcddf15f68193fa6295
suzumiyayuhi <923051761@qq.com>
2017-06-14 17:32:51 +0800

    2017.6.14
    2017.6.14

---
+++ hdoj/Problem Achieve/5491 The Next.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+using namespace std;
+char z1[32],z2[32];
+int main()
+{
+	int x,y=0;
+	cin>>x;
+	while(c--,y++)
+	{
+		cout<<"Case #"<<y<<": ";
+		long long A,B,s1,s2;
+		int c1=0,c2=0;
+		cin>>A>>s1>>s2;
+		while(1)
+		{
+			
+		}
+	}
+}
