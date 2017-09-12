618e0a2f4c09e5fa3719b9b2932b41fb6d6ff7d3
suzumiyayuhi <923051761@qq.com>
2017-09-12 13:35:53 +0800

    2017.9.12
    2017.9.12

---
+++ hdoj/Problem Achieve/2366 space.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+const int MAXN=101;
+int xi[MAXN],yi[MAXN];
+int n,l,res;
+
+void check(int a)
+{
+	for(int i=a+1;i<=n;i++)
+	{
+		if(xi[i]-xi[a]>a||xi[i]-xi[a])
+		{
+			
+		}
+		else
+		{
+			int tx=xi[i]-xi[a];
+			int ty=yi[i]-yi[a];
+			double tem=sqrt(tx*tx+ty*ty);
+			if(tem<l)
+			{
+				res++;
+			}
+		}	
+	}
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		res=0;
+		cin>>n>>l;
+		for(int i=1;i<=n;i++)
+			cin>>xi[i]>>yi[i];
+		for(int i=1;i<n;i++)
+			check(i);
+		cout<<res<<endl;
+	}
+}
