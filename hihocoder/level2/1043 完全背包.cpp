eb5963674482b8afd18e50653d5ab987f63c7275
suzumiyayuhi <923051761@qq.com>
2017-09-25 17:42:26 +0800

    2017.9.25
    2017.9.25

---
+++ hihocoder/level2/1043 完全背包.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int MAXN=1000001;
+int n,m;
+int weight[MAXN],value[MAXN],f[MAXN];
+
+int main()
+{
+	cin>>n>>m;
+	memset(f,0,sizeof(f));
+	for(int i=1;i<=n;i++)
+		cin>>weight[i]>>value[i];
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=m;j++)
+			if(weight[i]<=j)
+				f[j]=max(f[j],f[j-weight[i]]+value[i]);
+	}
+	cout<<f[m]<<endl;
+}
